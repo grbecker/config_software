1c9e189b572f16cb47bde089595d624e2e33c5d2
grbecker <guilhermebeckerpsi@gmail.com>
2021-06-27 01:36:38 -0300

    teste php unit

--- tests/Testes.php
+++ tests/Testes.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 class Testes extends TestCase {
     //teste 1
     public function testValidarEmail() {
-        $email = new Email();
-        $email->validarEmail("guilherme@psinf.com.br");
+        $email = new Email();        
+		$email->assertEquals(true, $email->validarEmail("guilherme@psinf.com.br"));
     }
 }
