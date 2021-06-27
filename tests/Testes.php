3faf34680798d955263fc10b2e8d9c3fe373b06a
grbecker <guilhermebeckerpsi@gmail.com>
2021-06-27 01:50:18 -0300

    adicao de testes

--- tests/Testes.php
+++ tests/Testes.php
@@ -2,13 +2,36 @@
 
 include_once './classes/Email.php';
 
+include_once './util/FormatValues.php';
+include_once './util/TranslateClass.php';
+
 use PHPUnit\Framework\TestCase;
 
 class Testes extends TestCase {
+
     //teste 1
     public function testValidarEmail() {
         $email = new Email();        
 		$this->assertEquals(true, $email->validarEmail("guilherme@psinf.com.br"));
     }
+
+    //teste 2
+    public function testDataNormal() {
+        $data = new FormatValues();
+        $this->assertEquals('01/01/2020', $data->dataNormal("2020/01/01"));
+    }
+
+    //teste 3
+    public function testDataSQL() {
+        $data = new FormatValues();
+        $this->assertEquals("2020-01-01", $data->dataSQL("01/01/2020"));
+    }
+	
+    //teste 4
+    public function testTrasnlate() {
+        $form = new TranslateClass();
+        $data = $form->convert("LancamentoForm", "R");
         $this->assertEquals("Lançamento - Receita", $data);                
+    }    
+
 }
