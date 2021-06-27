2c3245baedb207939352679cbb159d9f395c308b
grbecker <guilhermebeckerpsi@gmail.com>
2021-06-27 16:21:01 -0300

    ajuste bd

--- db/Connection.php
+++ db/Connection.php
@@ -7,7 +7,7 @@ class Connection {
     public static function open() {
 
         if (empty(self::$conn)) {
-            self::$conn = new PDO("mysql:host=c107gastos.mysql.dbaas.com.br;port=3306;dbname=c107gastos", "c107gastos", "Senhapsi@1234");
+            self::$conn = new PDO("mysql:host=localhost;port=3306;dbname=gastos", "root", "12345678");
             self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return self::$conn;
         } else {
