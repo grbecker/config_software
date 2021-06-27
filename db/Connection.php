91d48d64dd909513c4af4213c6f30c6f71ea3523
grbecker <guilhermebeckerpsi@gmail.com>
2021-06-27 19:08:23 -0300

    ajustes bd

--- db/Connection.php
+++ db/Connection.php
@@ -7,7 +7,7 @@ class Connection {
     public static function open() {
 
         if (empty(self::$conn)) {
-            self::$conn = new PDO("mysql:host=localhost;port=3306;dbname=gastos", "root", "12345678");
+            self::$conn = new PDO("mysql:host=127.0.0.1;port=3307;dbname=gastos", "root", "");
             self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return self::$conn;
         } else {
